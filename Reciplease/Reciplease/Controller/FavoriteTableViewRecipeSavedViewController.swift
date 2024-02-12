4b9b8775301a01859f4ab65b0d1a6744801671ee
Flop79 <peyronyflorian.pro@gmail.com>
2024-02-12 10:21:18 +0100

    adding the VC for all the vues and working on design, and constraints.

--- Reciplease/Reciplease/Controller/FavoriteTableViewRecipeSavedViewController.swift
+++ Reciplease/Reciplease/Controller/FavoriteTableViewRecipeSavedViewController.swift
@@ -1,19 +1,17 @@
 //
-//  FavoriteRecipeViewController.swift
+//  FavoriteTableViewRecipeSavedViewController.swift
 //  Reciplease
 //
-//  Created by Florian Peyrony on 08/02/2024.
+//  Created by Florian Peyrony on 12/02/2024.
 //
 
 import UIKit
 
-class FavoriteRecipeViewController: UIViewController {
+class FavoriteTableViewRecipeSavedViewController: UIViewController {
 
-    @IBOutlet weak var returnButton: UINavigationItem!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        returnButton.titleView?.tintColor = #colorLiteral(red: 0.2, green: 0.8, blue: 0.5, alpha: 1.0)
+
         // Do any additional setup after loading the view.
     }
     
