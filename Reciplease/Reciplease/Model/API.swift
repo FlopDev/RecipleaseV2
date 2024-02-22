10468371c977bd675a362c4c72f29af0bcd94e3f
Flop79 <peyronyflorian.pro@gmail.com>
2024-02-22 09:58:59 +0100

    starting working on API model who fetch data to Edanam API, and DataModel

---
+++ Reciplease/Reciplease/Model/API.swift
@@ -0,0 +1,8 @@
+//
+//  API.swift
+//  Reciplease
+//
+//  Created by Florian Peyrony on 22/02/2024.
+//
+
+import Foundation
---
+++ Reciplease/Reciplease/Model/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  Reciplease
+//
+//  Created by Florian Peyrony on 22/02/2024.
+//
+
+import Foundation
